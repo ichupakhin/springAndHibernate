c7616aa43ba11bcdcc2fb1f054cb51a7a3fea85a
Ilia Chupakhin <ilia063@gmx.de>
2023-03-29 22:01:24 +0200

    Exception Handling

--- newCourse/projects/04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/luv2code/demo/rest/StudentRestController.java
+++ newCourse/projects/04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/luv2code/demo/rest/StudentRestController.java
@@ -1,9 +1,10 @@
 package com.luv2code.demo.rest;
 
 import com.luv2code.demo.entity.Student;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.annotation.PostConstruct;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,14 +13,31 @@ import java.util.List;
 @RequestMapping("/api")
 public class StudentRestController {
 
+    private List<Student> theStudents;
+
+    @PostConstruct
+    public void loadData()
+    {
+        theStudents = new ArrayList<>();
+        theStudents.add(new Student("Poornima", "Patel"));
+        theStudents.add(new Student("Mario", "Rossi"));
+        theStudents.add(new Student("Mary", "Smith"));
+
+    }
+
     @GetMapping("/students")
     public List<Student> getStudents()
     {
-        List<Student> students = new ArrayList<>();
-        students.add(new Student("Poornima", "Patel"));
-        students.add(new Student("Mario", "Rossi"));
-        students.add(new Student("Mary", "Smith"));
-        return students;
+        return theStudents;
     }
 
+    @GetMapping("/students/{studentId}")
+    public Student getStudent(@PathVariable int studentId)
+    {
+        if(studentId < 0 || studentId > theStudents.size())
+        {
+            throw new StudentNotFoundException("Student id not found - " + studentId);
+        }
+        return theStudents.get(studentId);
+    }
 }
