badbdc0f9e150501e93256fafa2eba7d258030e2
Ilia Chupakhin <ilia063@gmx.de>
2023-04-02 15:40:18 +0200

    Done with REST API

--- newCourse/projects/04-spring-boot-rest-crud/03-spring-boot-rest-crud-employee-with-spring-data-jpa/src/main/java/com/luv2code/cruddemo/service/EmployeeService.java
+++ newCourse/projects/04-spring-boot-rest-crud/03-spring-boot-rest-crud-employee-with-spring-data-jpa/src/main/java/com/luv2code/cruddemo/service/EmployeeService.java
@@ -1,10 +1,10 @@
-package com.luv2code.cruddemo.dao;
+package com.luv2code.cruddemo.service;
 
 import com.luv2code.cruddemo.entity.Employee;
 
 import java.util.List;
 
-public interface EmployeeDAO {
+public interface EmployeeService {
     List<Employee> findAll();
 
     Employee findById(int theId);
